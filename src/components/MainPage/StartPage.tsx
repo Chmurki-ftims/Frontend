c89dc2c73ef014e9133bddf7f5719eba7043cff0
Wm11 <56996174+Wm11@users.noreply.github.com>
2020-12-07 13:59:24 +0100

    poprawiona sciezka do pliku Nav.jsx

--- src/components/MainPage/StartPage.tsx
+++ src/components/MainPage/StartPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "E:/PL- informatyka/Vsemestr/Systemy przetwarzania w chmurze/kalkom_v2/src/components/NavBar/Nav";
+import NavBar from "../NavBar/Nav";
 import { PageContent } from "./PageContent";
 import "./MainPage.css";
 
